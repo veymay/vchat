149cad0f952d845485b771544fd61a032529115e
VEYMAY <veymayuz@gmail.com>
2023-08-05 17:58:52 +0500

    change bg

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ let text = document.querySelector('input')
 let admin = document.querySelector('.add>.admin')
 let client = document.querySelector('.add>.client')
 document.addEventListener('keydown', (e) => {
-  if (e.shiftKey && e.key == 'Enter' && text.value != '') {
+  if (e.shiftKey && e.key == 'Enter' && text.value !== '') {
     list.innerHTML += `<li class="client">${text.value.trim()}<span class="remove"><i class="fi fi-br-trash"></i></span></li>`
     text.value = ''
     list.scrollTo(0, list.scrollHeight);
-  } else if (e.key == "Enter" && text.value != '') {
+  } else if (e.key == "Enter" && text.value !== '') {
     list.innerHTML += `<li class="admin">${text.value.trim()}<span class="remove"><i class="fi fi-br-trash"></i></span></li>`
     text.value = ''
     list.scrollTo(0, list.scrollHeight);
@@ -19,17 +19,20 @@ document.addEventListener('keydown', (e) => {
 })
 
 admin.addEventListener('click', () => {
-  list.innerHTML += `<li class="admin animate__animated animate__slideInUp">${text.value.trim()}</li>`
-  console.log(text.value.trim());
-  text.value = ''
+  if (text.value !== '') {
+    list.innerHTML += `<li class="admin animate__animated animate__slideInUp">${text.value.trim()}</li>`
+    text.value=''
+  }
 })
 client.addEventListener('click', () => {
-  list.innerHTML+=`<li class="client">${text.value.trim()}</li>`
+  if (text.value !== '') {
+    list.innerHTML+=`<li class="client">${text.value.trim()}</li>`
+    text.value=''
+  }
 })
 listItem.forEach(item => {
   remove.addEventListener('click', (e) => {
     item.remove()
-    console.log('li');
     e.stopPropagation()
   })
 })

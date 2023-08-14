5ab5a1cdb4a1d8a5f77a210bfba5b8d6b14aa096
VEYMAY <veymayuz@gmail.com>
2023-08-14 18:27:10 +0500

    add save to localstorage

--- js/main.js
+++ js/main.js
@@ -4,38 +4,65 @@ let remove = document.querySelector('.list>li>.remove')
 let text = document.querySelector('input')
 let admin = document.querySelector('.add>.admin')
 let client = document.querySelector('.add>.client')
+
+let chatList = JSON.parse(localStorage.getItem('chatList')) ? JSON.parse(localStorage.getItem('chatList')) : []
+
+function setChat() {
+  localStorage.setItem('chatList', JSON.stringify(chatList))
+}
+
+function showChat() {
+  chatList.forEach((item, id) => {
+    if (item.admin) {
+      list.innerHTML += `
+      <li class="admin">
+        ${item.admin}
+        <span class="remove" onclick="deleteChat(${id})">
+          <i class="fi fi-br-trash"></i>
+        </span>
+      </li>`
+    }
+    if (item.client) {
+      list.innerHTML += `
+      <li class="client">
+        ${item.client}
+        <span class="remove" onclick="deleteChat(${id})">
+          <i class="fi fi-br-trash"></i>
+        </span>
+      </li>`
+    }
+  });
+}
+function deleteChat(id) {
+  chatList.splice(id, 1)
+  setChat()
+  list.innerHTML = ''
+  showChat()
+}
+function addAdmin() {
+  chatList.push({ admin: text.value.trim() })
+  setChat()
+  list.innerHTML = ''
+  showChat()
+  text.value = ''
+  list.scrollTo(0, list.scrollHeight);
+}
+function addClient() {
+  chatList.push({ client: text.value.trim() })
+  setChat()
+  list.innerHTML = ''
+  showChat()
+  text.value = ''
+  list.scrollTo(0, list.scrollHeight);
+}
+
+
 document.addEventListener('keydown', (e) => {
   if (e.shiftKey && e.key == 'Enter' && text.value !== '') {
-    list.innerHTML += `<li class="client">${text.value.trim()}<span class="remove"><i class="fi fi-br-trash"></i></span></li>`
-    text.value = ''
-    list.scrollTo(0, list.scrollHeight);
+    addClient()
   } else if (e.key == "Enter" && text.value !== '') {
-    list.innerHTML += `<li class="admin">${text.value.trim()}<span class="remove"><i class="fi fi-br-trash"></i></span></li>`
-    text.value = ''
-    list.scrollTo(0, list.scrollHeight);
-    listItem = document.querySelectorAll('.list>li')
+    addAdmin()
   }
   text.focus()
 })
-
-admin.addEventListener('click', () => {
-  if (text.value !== '') {
-    list.innerHTML += `<li class="admin animate__animated animate__slideInUp">${text.value.trim()}</li>`
-    text.value=''
-  }
-})
-client.addEventListener('click', () => {
-  if (text.value !== '') {
-    list.innerHTML+=`<li class="client">${text.value.trim()}</li>`
-    text.value=''
-  }
-})
-listItem.forEach(item => {
-  remove.addEventListener('click', (e) => {
-    item.remove()
-    e.stopPropagation()
-  })
-})
-list.addEventListener('click', () => {
-  console.log('ul');
-})
+showChat()
